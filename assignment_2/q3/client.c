f29ca4588288ade5193f710d48d2ea9611e9c097
mauryakshitij <kkmaurya2004@gmail.com>
2023-09-10 19:12:00 +0530

    add q3

---
+++ assignment_2/q3/client.c
@@ -0,0 +1,122 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+int sockfd = 0; // Socket file descriptor for communication with the server
+char id[2];     // Unique identifier for the client
+pthread_t tid1, tid2; // Thread IDs for reading and writing messages
+
+// Function to read messages from the server
+void *read_msg(void *arg)
+{
+    char str[100];
+    read(sockfd, str, 100);
+
+    // Check if the received message is "exit"
+    if (strcmp(str, "exit") == 0)
+    {
+        printf("OK Bye!\n");
+        close(sockfd);
+        exit(0);
+    }
+
+    // Print the message received from the server
+    printf("%c says: %s\n", str[99], str + 2);
+
+    // Create a new thread to continue reading messages
+    pthread_create(&tid1, NULL, read_msg, NULL);
+    return (NULL);
+}
+
+// Function to write messages to the server
+void *write_msg(void *arg)
+{
+    char str[100];
+    fflush(stdout);
+    printf("Enter message: \n");
+    scanf("%[^\n]%*c", str);
+    str[99] = id[0];
+    write(sockfd, str, 100);
+
+    // Check if the entered message is "exit"
+    if (strcmp(str, "exit") == 0)
+    {
+        close(sockfd);
+        exit(0);
+    }
+
+    // Create a new thread to continue writing messages
+    pthread_create(&tid2, NULL, write_msg, NULL);
+    return (NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    int n = 0;
+    char recvBuff[1024];
+    int temp;
+    struct sockaddr_in serv_addr;
+    char s[100];
+
+    if (argc != 2)
+    {
+        printf("\n Usage: %s <ip of server> \n", argv[0]);
+        return 1;
+    }
+
+    memset(recvBuff, '0', sizeof(recvBuff));
+
+    // Create a socket for communication
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Error : Could not create socket \n");
+        return 1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(5000);
+
+    // Convert the server IP address from text to binary
+    printf("Server present at the IP address: %s\n", argv[1]);
+    if (inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0)
+    {
+        printf("\n inet_pton error occurred\n");
+        return 1;
+    }
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\n Error : Connect Failed \n");
+        return 1;
+    }
+    printf("\nSuccessful connection with server has been established\n");
+
+    // Read the unique client identifier from the server
+    read(sockfd, id, 2);
+    printf("My ID : %c\n", id[0]);
+
+    // Instructions for sending messages
+    printf("Please follow the given instructions for sending messages-\n");
+    printf("1. To send a message to all clients connected to the server, the format of the message should be * <message>\n");
+    printf("2. To send a message to a particular client, the format of the message should be <Client ID> <message>\n");
+    printf("3. To exit the chat room, enter 'exit'\n");
+
+    // Create threads for reading and writing messages
+    pthread_create(&tid1, NULL, read_msg, NULL);
+    pthread_create(&tid2, NULL, write_msg, NULL);
+
+    // Keep the main thread running
+    while (1);
+    return 0;
+}
---
+++ assignment_2/q3/server.c
@@ -0,0 +1,132 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <pthread.h>
+
+#define MAX 5
+int connfd[MAX] = {0};
+int i, e[MAX] = {0};
+pthread_t tid2;
+pthread_t tid[MAX];
+
+// Function to read messages from clients
+void *read_msg(void *arg)
+{
+    int k = *(int *)arg;
+    int l;
+    char str[100];
+    read(connfd[k], str, 100);
+
+    // Check if the received message is "exit"
+    if (strcmp(str, "exit") == 0)
+    {
+        printf("Client %c is Offline\n", (char)(65 + k));
+        close(connfd[k]);
+        return (NULL);
+    }
+
+    // Check if the message is intended for all clients
+    if (str[0] == '*')
+    {
+        for (l = 0; l < i; l++)
+            if (l != k)
+                write(connfd[l], str, 100);
+
+        printf("%c says: %s\n", str[99], str);
+
+        // Create a new thread to continue reading messages
+        pthread_create(&tid[k], NULL, read_msg, (void *)&k);
+    }
+    else // Message is intended for a specific client
+    {
+        for (l = 0; l < i; l++)
+            if (l == (str[0] - 'A'))
+                write(connfd[l], str, 100);
+
+        // Create a new thread to continue reading messages
+        pthread_create(&tid[k], NULL, read_msg, (void *)&k);
+    }
+    return (NULL);
+}
+
+// Function to send messages to clients
+void *write_msg(void *arg)
+{
+    int l;
+    char str[100];
+    fflush(stdin);
+    scanf("%[^\n]%*c", str);
+    str[99] = 'S';
+
+    // Send the message to all clients
+    for (l = 0; l < i; l++)
+        write(connfd[l], str, 100);
+
+    // Check if the entered message is "exit" to exit the program
+    if (strcmp(str, "exit") == 0)
+        exit(0);
+
+    // Create a new thread to continue writing messages
+    pthread_create(&tid2, NULL, write_msg, NULL);
+    return (NULL);
+}
+
+int main(int argc, char *argv[])
+{
+    int listenfd = 0;
+    struct sockaddr_in serv_addr;
+    char sendBuff[1025];
+    int temp;
+    time_t ticks;
+    char C_name[MAX][2];
+    int j[MAX];
+
+    // Create a socket for server communication
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000);
+
+    // Bind the socket to the server address
+    bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    // Start listening for incoming client connections
+    listen(listenfd, 10);
+
+    // Initialize client names and thread IDs
+    for (i = 0; i < MAX; ++i)
+    {
+        C_name[i][0] = (char)(65 + i); // Client names A, B, C, ...
+        C_name[i][1] = '\n';           // Newline character for printing
+        j[i] = i;                      // Index used for thread creation
+    }
+
+    printf("My ID : S\n");
+
+    // Create a thread for writing messages from the server
+    pthread_create(&tid2, NULL, write_msg, NULL);
+
+    // Accept connections from clients and create threads for each client
+    for (i = 0; i < MAX; i++)
+    {
+        connfd[i] = accept(listenfd, (struct sockaddr *)NULL, NULL);
+
+        // Send the client its name (A, B, C, ...) upon successful connection
+        write(connfd[i], C_name[i], 2);
+
+        printf("Successful connection with Client %c has been established\n", C_name[i][0]);
+
+        // Create a thread for reading messages from this client
+        pthread_create(&tid[i], NULL, read_msg, (void *)(j + i));
+    }
+}
